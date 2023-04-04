de5af3abe3c83a79a7e846dde637ab6f739bedd3
slazarska <slazarska@protonmail.com>
2023-04-04 17:14:24 +0300

    refactored tests: added allure steps

--- tests/test_login.py
+++ tests/test_login.py
@@ -38,45 +38,59 @@ class TestLogin:
     @allure.severity(Severity.BLOCKER)
     @allure.story('Authorization')
     def test_login(self):
-        login_user = Authentication().get_login_user(valid_creds)
-        login_user.should_have_status_code(200)
-        login_user.should_have_body_field("code", 200)
-        login_user.does_str_in_value("message", "logged in user session:")
+        with allure.step('Login with valid credentials'):
+            login_user = Authentication().get_login_user(valid_creds)
+
+        with allure.step('Check login is successful'):
+            login_user.should_have_status_code(200)
+            login_user.should_have_body_field("code", 200)
+            login_user.does_str_in_value("message", "logged in user session:")
 
     @allure.severity(Severity.CRITICAL)
     @allure.story('Authorization')
     def test_logout(self, login_user):
-        logout_user = User().get_logout_user()
-        logout_user.should_have_status_code(200)
-        logout_user.should_have_body_field("code", 200)
-        logout_user.should_have_body_field("message", "ok")
+        with allure.step('Logout'):
+            logout_user = User().get_logout_user()
+
+        with allure.step('Check logout is successful'):
+            logout_user.should_have_status_code(200)
+            logout_user.should_have_body_field("code", 200)
+            logout_user.should_have_body_field("message", "ok")
 
     @allure.severity(Severity.NORMAL)
     @allure.story('Authorization')
     def test_login_with_no_credentials(self):
-        login_user = Authentication().get_login_user(empty_creds)
-        login_user.should_have_status_code(400)
-        login_user.should_have_body_field("code", 400)
+        with allure.step('Login with empty credentials'):
+            login_user = Authentication().get_login_user(empty_creds)
+        with allure.step('Check login with empty credentials is failed'):
+            login_user.should_have_status_code(400)
+            login_user.should_have_body_field("code", 400)
 
     @allure.severity(Severity.NORMAL)
     @allure.story('Authorization')
     def test_login_with_invalid_credentials(self):
-        login_user = Authentication().get_login_user(invalid_creds)
-        login_user.should_have_status_code(400)
-        login_user.should_have_body_field("code", 400)
+        with allure.step('Login with invalid credentials'):
+            login_user = Authentication().get_login_user(invalid_creds)
+        with allure.step('Check login with invalid credentials is failed'):
+            login_user.should_have_status_code(400)
+            login_user.should_have_body_field("code", 400)
 
     @allure.severity(Severity.CRITICAL)
     @allure.story('Create a new user')
     def test_create_user(self, delete_user):
-        create_user = User().post_add_user(created_user)
-        create_user.should_have_status_code(200)
-        create_user.should_have_body_field("code", 200)
-        create_user.should_have_body_field("message", str(created_user["id"]))
+        with allure.step('Add a new user'):
+            create_user = User().post_add_user(created_user)
+        with allure.step('Check the new user is successfully created'):
+            create_user.should_have_status_code(200)
+            create_user.should_have_body_field("code", 200)
+            create_user.should_have_body_field("message", str(created_user["id"]))
 
     @allure.severity(Severity.NORMAL)
     @allure.story('Deleting the user')
     def test_delete_user(self, create_user):
-        delete_user = User().delete_user(created_user["username"])
-        delete_user.should_have_status_code(200)
-        delete_user.should_have_body_field("code", 200)
-        delete_user.should_have_body_field("message", str(created_user["username"]))
+        with allure.step('Delete the user'):
+            delete_user = User().delete_user(created_user["username"])
+        with allure.step('Check the user is successfully deleted'):
+            delete_user.should_have_status_code(200)
+            delete_user.should_have_body_field("code", 200)
+            delete_user.should_have_body_field("message", str(created_user["username"]))
--- tests/test_pet.py
+++ tests/test_pet.py
@@ -15,38 +15,52 @@ class TestPet:
     @allure.severity(Severity.CRITICAL)
     @allure.story('Adding a new pet')
     def test_add_pet_with_valid_data(self, delete_pet):
-        add_new_pet = Pet().post_add_new_pet(pet_data)
-        add_new_pet.should_have_status_code(200)
-        add_new_pet.should_have_body_field("id", pet_data["id"])
-        add_new_pet.should_have_body_field("category", pet_data["category"])
-        add_new_pet.should_have_body_field("name", pet_data["name"])
-        add_new_pet.should_have_body_field("status", pet_data["status"])
+        with allure.step('Add a new pet'):
+            add_new_pet = Pet().post_add_new_pet(pet_data)
+
+        with allure.step('Check the new pet is successfully created'):
+            add_new_pet.should_have_status_code(200)
+            add_new_pet.should_have_body_field("id", pet_data["id"])
+            add_new_pet.should_have_body_field("category", pet_data["category"])
+            add_new_pet.should_have_body_field("name", pet_data["name"])
+            add_new_pet.should_have_body_field("status", pet_data["status"])
 
     @allure.severity(Severity.NORMAL)
     @allure.story('Find the pet')
     def test_find_pet_by_id(self, add_pet):
-        get_pet_by_id = Pet().get_pet_by_id(pet_data["id"])
-        get_pet_by_id.should_have_status_code(200)
-        get_pet_by_id.should_have_body_field("name", pet_data["name"])
-        get_pet_by_id.should_have_body_field("status", pet_data["status"])
+        with allure.step('Find the pet by id'):
+            get_pet_by_id = Pet().get_pet_by_id(pet_data["id"])
+
+        with allure.step('Check the pet is successfully found'):
+            get_pet_by_id.should_have_status_code(200)
+            get_pet_by_id.should_have_body_field("name", pet_data["name"])
+            get_pet_by_id.should_have_body_field("status", pet_data["status"])
 
     @allure.severity(Severity.NORMAL)
     @allure.story('Deleting the pet')
     def test_delete_pet(self, add_pet):
-        delete_pet = Pet().delete_pet(pet_data)
-        delete_pet.should_have_status_code(200)
-        delete_pet.should_have_body_field("code", 200)
-        delete_pet.should_have_body_field("message", str(pet_data["id"]))
+        with allure.step('Delete the pet'):
+            delete_pet = Pet().delete_pet(pet_data)
+
+        with allure.step('Check the pet is successfully deleted'):
+            delete_pet.should_have_status_code(200)
+            delete_pet.should_have_body_field("code", 200)
+            delete_pet.should_have_body_field("message", str(pet_data["id"]))
 
     @allure.severity(Severity.NORMAL)
     @allure.story('Changing the pet')
     def test_update_existing_pet(self, add_pet, delete_pet):
-        update_existing_pet = Pet().put_existing_pet(new_pet_data)
-        update_existing_pet.should_have_status_code(200)
-        update_existing_pet.should_have_body_field("name", new_pet_data["name"])
-        update_existing_pet.should_have_body_field("status", new_pet_data["status"])
+        with allure.step('Update the existing pets data'):
+            update_existing_pet = Pet().put_existing_pet(new_pet_data)
 
-        get_pet_by_id = Pet().get_pet_by_id(new_pet_data["id"])
-        get_pet_by_id.should_have_status_code(200)
-        get_pet_by_id.should_have_body_field("name", new_pet_data["name"])
-        get_pet_by_id.should_have_body_field("status", new_pet_data["status"])
+        with allure.step('Check the pets data is successfully updated'):
+            update_existing_pet.should_have_status_code(200)
+            update_existing_pet.should_have_body_field("name", new_pet_data["name"])
+            update_existing_pet.should_have_body_field("status", new_pet_data["status"])
+
+        with allure.step('Find the pet by id'):
+            get_pet_by_id = Pet().get_pet_by_id(new_pet_data["id"])
+
+        with allure.step('Check the new pets data'):
+            get_pet_by_id.should_have_body_field("name", new_pet_data["name"])
+            get_pet_by_id.should_have_body_field("status", new_pet_data["status"])
